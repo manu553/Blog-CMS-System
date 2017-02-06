5835dc3602a99bc94b39d0745d4fdba0d5b0b3c9
Manveer Bhangu <manu.bhangu5530@gmail.com>
2017-02-06 15:45:21 -0800

    Initial setup
    Signed-off-by: Manveer Bhangu <manu.bhangu5530@gmail.com>

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+
+include "includes/header.php";
+
+?>
+    <!-- Navigation -->
+    <?php
+
+    include "includes/nav.php";
+
+    ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+
+                <?php
+
+                $query = "SELECT * FROM posts";
+                $posts_query = mysqli_query($connection, $query);
+
+                while($row = mysqli_fetch_assoc($posts_query)) 
+                {
+                    $post_id = $row['post_id'];
+                    $post_cat_id = $row['post_cat_id'];
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date = $row['post_date'];
+                    $post_image = $row['post_image'];
+                    $post_content = $row['post_content'];
+                    $post_tags = $row['post_tags'];
+                    $post_comment_count = $row['post_comment_count'];
+                    $post_status = $row['post_status'];
+
+                ?>
+
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
+
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="#"><?php echo $post_title ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href="index.php"><?php echo $post_author ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+                <hr>
+                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                <hr>
+                <p><?php echo $post_content ?></p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                <hr>
+
+                <?php 
+                } // ends while loops for posts
+                ?>
+
+
+            </div>
+
+            <!-- Blog Sidebar Widgets Column -->
+            <?php
+
+            include "includes/sidebar.php";
+
+            ?>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+<?php
+
+include "includes/footer.php";
+
+?>
